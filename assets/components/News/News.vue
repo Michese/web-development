daddffce51f2f6eaa1800a9c884d640ca93de77e
Michese <michese48@mail.ru>
2022-04-26 22:06:02 +0300

    remove carousel

--- assets/components/News/News.vue
+++ assets/components/News/News.vue
@@ -4,27 +4,27 @@
       <mim-spinner/>
     </div>
     <div v-else class="news">
-      <div id="carouselExampleControls" class="news__carousel carousel slide mt-2 mb-2" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="./images/1.jpg" class="carousel__image d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="./images/2.jpg" class="carousel__image d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="./images/3.jpg" class="carousel__image d-block w-100" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+<!--      <div id="carouselExampleControls" class="news__carousel carousel slide mt-2 mb-2" data-bs-ride="carousel">-->
+<!--        <div class="carousel-inner">-->
+<!--          <div class="carousel-item active">-->
+<!--            <img src="./images/1.jpg" class="carousel__image d-block w-100" alt="...">-->
+<!--          </div>-->
+<!--          <div class="carousel-item">-->
+<!--            <img src="./images/2.jpg" class="carousel__image d-block w-100" alt="...">-->
+<!--          </div>-->
+<!--          <div class="carousel-item">-->
+<!--            <img src="./images/3.jpg" class="carousel__image d-block w-100" alt="...">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">-->
+<!--          <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--          <span class="visually-hidden">Previous</span>-->
+<!--        </button>-->
+<!--        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">-->
+<!--          <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--          <span class="visually-hidden">Next</span>-->
+<!--        </button>-->
+<!--      </div>-->
 
       <div class="cards d-grid">
         <news-card v-for="(newItem, index) in news" :key="`new_${index}`" :new-item="newItem"/>
@@ -58,12 +58,6 @@ export default {
 .spinner-container {
   height: 300px;
 }
-
-//.news {
-//  &__carousel {
-//    max-height: 400px;
-//  }
-//}
 
 .carousel {
   &__image {
